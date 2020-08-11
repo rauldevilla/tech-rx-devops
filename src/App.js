bcf0f463705d22ea68843db20d0beadd3c2c58c4
rauldevilla <raul.devilla@gmail.com>
2020-08-11 16:12:56 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Landing from './components/Landing';
+import Dashboard from './components/Dashboard';
+
 function App() {
+  const [user, setUser] = useState(false);
+
+  const handleLogin = e => {
+    e.preventDefault();
+    setUser(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Route exact path='/' handleLogin={handleLogin} render={props => <Landing {...props} user={user.toString()} handleLogin={handleLogin} />} />
+        <Route exact path='/dashboard' component={Dashboard} />
+      </Router>
     </div>
   );
 }
